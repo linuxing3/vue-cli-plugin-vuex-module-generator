8986e80d5adf6363c75fb922904d9d6bd740a9bc
wenju xing <linuxing3@qq.com>
2018-09-18 08:31:31 -0400

    Update getters.js

--- generator/templates/module/getters.js
+++ generator/templates/module/getters.js
@@ -1,2 +1,10 @@
 export default {
+  moduleName: (state) => state.name,
+  itemCounts: (state) => state.items.length,
+  itemKeys: (state) => {
+    return ObjectKeysToArray(state.currentItem);
+  },
+  itemFiltered: (state) => {
+    return baseFilter(state.items, state.sortKey, state.filterKey);
+  },
 }
