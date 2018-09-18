a67ecff8c779359fb46b5216e52597cbc3f15d76
wenju xing <linuxing3@qq.com>
2018-09-18 10:20:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-module.exports = (api, options) => {
-  api.chainWebpack(webpackConfig => {}),
-  api.configWebpack(webpackConfig => {}),
-  api.registerCommand('vuexmodule', arg -> {})
+module.exports = (api, projectOptions) => {
+  api.chainWebpack(webpackConfig => {
+    // modify webpack config with webpack-chain
+  })
+
+  api.configureWebpack(webpackConfig => {
+    // modify webpack config
+    // or return object to be merged with webpack-merge
+  })
+
+  api.registerCommand('vuexmodule', args => {
+    // register `vue-cli-service test`
+  })
 }
