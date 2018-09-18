ca99600fc437fcf2254c096f55055e10b271caa3
wenju xing <linuxing3@qq.com>
2018-09-18 09:12:54 -0400

    Create types.js

---
+++ generator/templates/types.js
@@ -0,0 +1,57 @@
+/**
+ * vuex pathified types
+ */
+export const SET_ITEMS = "SET_ITEMS";
+export const SET_CURRENTITEMS = "SET_CURRENTITEM";
+export const SET_FILTER = "SET_FILTER";
+export const SET_NAME = "SET_NAME";
+
+export const setName = "setName";
+export const setItems = "setItems";
+export const setCurrentItem = "setCurrentItem";
+export const setFilter = "setFilter";
+
+/**
+ * All mutation notations
+ */
+export const mCreate = "mutationCreate";
+export const mDelete = "mutationDelete";
+export const mUpdate = "mutationUpdate";
+export const mRead = "mutationRead";
+export const mReadOne = "mutationReadOne";
+
+/**
+ * Useful helpers
+ */
+export const mSetValue = "mutationSetValue";
+export const mSetFilter = "mutationSetFilter";
+export const mSetToken = "mutationSetToken";
+
+export const mDbCreate = "mutationDbCreate";
+export const mDbDelete = "mutationDbDelete";
+
+export const mSetCurrent = "mutationSetCurrent";
+export const mCheckCurrentCollection = "mutationCheckCurrenCollection";
+
+/**
+ * All action notations
+ */
+export const aCreate = "actionCreate";
+export const aDelete = "actionDelete";
+export const aUpdate = "actionUpdate";
+export const aRead = "actionRead";
+export const aReadOne = "actionReadOne";
+export const aSet = "actionSet";
+export const aCheckCurrentCollection = "actionCheckCurrenCollection";
+
+/**
+ * All Namespace notations
+ */
+export const nsDatabase = "database";
+export const nsUser = "user";
+export const nsDocument = "document";
+export const nsWriter = "writer";
+export const nsTrello = "trello";
+export const nsBookmark = "bookmark";
+export const nsAccount = "account";
+export const nsNote = "note";
