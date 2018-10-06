c8258289be41e5f19d8c440249930d48eae6fdfc
linuxing3 <linuxing3@qq.com>
2018-10-06 12:24:23 -0400

    Quick commit to master

--- generator/templates/router/moduleRoute.ts
+++ generator/templates/router/moduleRoute.ts
@@ -1,8 +1,8 @@
 export default {
-    path: "/activity",
-    name: "Activity",
-    component: () =>
-      import(/* webpackChunkName: "routes" */
-      /* webpackMode: "lazy" */
-      `@/components/Activity/ActivityTable.vue`),
-  },
+  path: "/activity",
+  name: "Activity",
+  component: () =>
+    import(/* webpackChunkName: "routes" */
+    /* webpackMode: "lazy" */
+    `@/components/Activity/ActivityTable.vue`),
+};
